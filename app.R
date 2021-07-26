cdd68dc7e12bb1f4946d40dddd022abbebf8db18
Ed Ivimey-Cook <E.Ivimey-Cook@uea.ac.uk>
2021-07-26 14:56:26 +0100

    New UI

--- app.R
+++ app.R
@@ -1,14 +1,12 @@
 #load packages
 library(shiny)
 library(shinyWidgets)
-library(shinyalert)
-library(shinyjs)
 library(shinyFiles)
 library(magick)
 library(metaDigitise)
 library(fresh)
 library(shinythemes)
-
+library(bslib)
 
 if(Sys.info()["user"]=="joelpick"){
 	dir <- "/Users/joelpick/Desktop/images"
@@ -26,76 +24,103 @@ details <- get_notDone_file_details(dir)
 counter_total <- length(details$paths)
 
 ui <- fluidPage(
-    useShinyalert(),
-    useShinyjs(),
-    theme = shinytheme("sandstone"),
-    titlePanel(title=div(img(src="shiny.jpg", height = 100)), windowTitle = "shinyDigitise"),
-
+    tags$head(
+      tags$style(".buttonagency .bttn-primary{background-color: darkblue;}"),
+      tags$style(type = 'text/css',".myclass1 {background-color: #EFF8CD;}"),
+      tags$style(type = 'text/css',".myclass2 {background-color: #EFF8CD;}"),
+      tags$style(type = 'text/css',".myclass3 {background-color: #EFF8CD;}"),
+      tags$style(type = 'text/css',".myclass4 {background-color: #EFF8CD;}"),
+      tags$style(type = 'text/css',".myclass5 {background-color: #c9d7e8;}")
+    ),
+    theme = bs_theme(fg = "#3F1010", primary = "#332C50", base_font = "Arial", 
+                                     font_scale = NULL, `enable-shadows` = TRUE, spacer = "0.8rem", 
+                                     bootswatch = "flatly", bg = "#FFFFFF"),
     fluidRow(
-			tags$head(tags$script('$(document).on("shiny:connected", function(e) {
-                            Shiny.onInputChange("innerWidth", window.innerWidth);
-                            });
-                            $(window).resize(function(e) {
-                            Shiny.onInputChange("innerWidth", window.innerWidth);
-                            });
-                            ')),
-        column(3,
+    titlePanel(title=div(img(src="shiny.jpg", height = 80),"shinyDigitise"), windowTitle = "shinyDigitise"),
+        column(2, wellPanel(
           # shinyDirButton('dir', 'Select a folder', 'Please select a folder', FALSE),
           # verbatimTextOutput("dir", placeholder = TRUE),
-          htmlOutput("progress")
-        ),
-        column(3,
-          awesomeRadio(
-              inputId = "ShowOnlyNew",
-              label = "Show Only Unprocessed Images:",
-              choices = c("Yes", "No"),
-              status = "warning",
-              inline=TRUE
+          progressBar(id = "progress", value = 0, status = "danger", striped = TRUE, display_pct = T)
+        )),
+    column(3,wellPanel(
+      # checkboxGroupButtons(
+      #     inputId = "Next",
+      #     choices = c("Next", "Previous"),
+      #     status = "danger"
+      # )
+      div(class = "buttonagency",
+      actionBttn(
+        inputId = "Previous",
+        label = "Previous", 
+        style = "material-flat",
+        color = "primary",
+      ),
+      actionBttn(
+        inputId = "Next",
+        label = "Next", 
+        style = "material-flat",
+        color = "primary"
+      ))
+      
+    )),
+    
+        column(2, wellPanel(
+          strong("Show only unprocessed images:"), class = "myclass1", id = "myid1",
+          prettyToggle(
+            inputId = "ShowOnlyNew",
+            label_on = "Yes", 
+            icon_on = icon("check"),
+            status_on = "info",
+            status_off = "warning", 
+            label_off = "No",
+            icon_off = icon("remove")
           )
-        ),
-        column(3,
-          textInput(inputId="general_cex",label="Point Size",value=1)
-        )
-
+        )),
+    
+        column(2, wellPanel(
+               sliderTextInput(
+                 inputId = "general_cex",
+                 label = strong("Point Size:"), 
+                 choices = c(seq(from = 0.1, to = 3, by = 0.1)),
+                 grid = F)
+        ))
     ),
-
     # Sidebar with a slider input for number of bins
-    fluidRow(
-
-      column(3,
-        wellPanel(
-            # checkboxGroupButtons(
-            #     inputId = "Next",
-            #     choices = c("Next", "Previous"),
-            #     status = "danger"
-            # )
-            actionButton(inputId = "Previous",
-                         label = "Previous"),
-            actionButton(inputId = "Next",
-                         label = "Next"),
-
-          ),
+    sidebarLayout(
+      sidebarPanel( width = 4,
+        id = "tPanel",style = "overflow-y:scroll; max-height: 600px; position:relative;",
 
         ## I've changed this because flip and rotate are different processes, so need two buttons
         wellPanel(
+          strong("Image adjust:"), class = "myclass2", id = "myid1",
+          br(),
             # checkboxGroupButtons(
             #     inputId = "Orient",
             #     label = h3("Orientation:"),
             #     choices = c("Flip", "Rotate"),
             #     status = "danger"
             # )
-            actionButton(inputId = "flip",
-                         label = "Flip"),
-            actionButton(inputId = "rotate",
-                         label = "Rotate"),
+          div(class = "buttonagency",
+          actionBttn(
+            inputId = "Flip",
+            label = "Flip",
+            style = "material-flat", 
+            color = "primary"
+          ),
+          actionBttn(
+            inputId = "Rotate",
+            label = "Rotate",
+            style = "material-flat", 
+            color = "primary"
+          ))
           ),
 
           wellPanel(
-            awesomeRadio(
+            pickerInput(
                 inputId = "PlotType",
-                label = h3("Plot type:"),
+                label = h6(strong("Plot type:")),
                 choices = c("Mean/error", "Scatterplot", "Histogram", "Boxplot"),
-                status = "primary",
+                
                 inline=TRUE)
               ),
 
@@ -138,7 +163,7 @@ ui <- fluidPage(
                             br(),
 	                         awesomeRadio(
 	                             inputId = "errortype",
-	                             label = h3("Type of error"),
+	                             label = h6(strong("Type of error")),
 	                             choices = c("SE", "95%CI", "SD"),
 	                             status = "warning",
 	                             inline=TRUE
@@ -193,7 +218,7 @@ ui <- fluidPage(
                  awesomeCheckbox(
                    inputId = "Id005",
                    label = "Logged values?",
-                   value = TRUE,
+                   value = F,
                    status = "danger"),
 
 
@@ -201,18 +226,33 @@ ui <- fluidPage(
             )
           ),
           wellPanel(
-              checkboxGroupButtons(
-                  inputId = "grouping",
-                  label = h3("Groups"),
-                  choices = c("Add", "Delete"),
-                  status = "royal"
+            strong("Groups:"), class = "myclass3", id = "myid1",
+            br(),
+            div(class = "buttonagency",
+            actionBttn(
+              inputId = "Add",
+              label = "Add", 
+              style = "material-flat",
+              color = "primary"
+            ),
+            actionBttn(
+              inputId = "delete",
+              label = "Delete", 
+              style = "material-flat",
+              color = "primary"
+            ))
               )
-            )
+          
 
         ),
-        column(6,mainPanel(
-        plotOutput("metaPlot", click="plot_click")
-    ))
+mainPanel(
+        plotOutput("metaPlot", 
+                   click="plot_click",
+                   dblclick = "plot_dblclick",
+                   hover = "plot_hover",
+                   brush = "plot_brush"),
+        verbatimTextOutput("info")
+    )
       )
     )
 
@@ -220,7 +260,6 @@ ui <- fluidPage(
 
 
 server <- function(input, output, session) {
-
 #dirchoose function
   # shinyDirChoose(
   #   input,
@@ -286,9 +325,10 @@ output$metaPlot <- renderPlot({
   image <- image_read(images[counter$countervalue])
 	plot(image)
 }
+
 	# list(src = image, contentType = "image/png")
 # } ,
-#     height=reactive(ifelse(!is.null(input$innerWidth),input$innerWidth*3/5,0))
+#height=reactive(ifelse(!is.null(input$innerWidth),input$innerWidth*3/5,0))
 )
 
 
@@ -324,8 +364,27 @@ observeEvent(counter$countervalue, {
 
 output$progress <- renderText({
     percent = round(counter$countervalue/counter_total*100, 0)
-    paste0("<font color=\"#ff3333\"><b>",percent,"%", " ", "screened","</b></font>")
+    paste0("<font color=\"#ff3333\"><b>",percent,"%", " ", "extracted","</b></font>")
 
+})
+
+output$info <- renderText({
+  xy_str <- function(e) {
+    if(is.null(e)) return("NULL\n")
+    paste0("x=", round(e$x, 1), " y=", round(e$y, 1), "\n")
+  }
+  xy_range_str <- function(e) {
+    if(is.null(e)) return("NULL\n")
+    paste0("xmin=", round(e$xmin, 1), " xmax=", round(e$xmax, 1), 
+           " ymin=", round(e$ymin, 1), " ymax=", round(e$ymax, 1))
+  }
+  
+  paste0(
+    "click: ", xy_str(input$plot_click),
+    "dblclick: ", xy_str(input$plot_dblclick),
+    "hover: ", xy_str(input$plot_hover),
+    "brush: ", xy_range_str(input$plot_brush)
+  )
 })
 
 
