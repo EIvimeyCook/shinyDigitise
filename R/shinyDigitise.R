42219b6ceda6a48a3a1d89f891f7de27a0627264
Joel Pick <joel.l.pick@gmail.com>
2022-01-29 22:13:41 +0000

    allow specific filenames to be entered

--- R/shinyDigitise.R
+++ R/shinyDigitise.R
@@ -2,21 +2,40 @@
 #' @description Single or batch processing of figures with .png, .jpg, .tiff, .pdf extensions within a set directory. metaDigitise() consolidates the data and exports the data for each image and image type. It can also summarise the data, provide the raw data (if scatterplots) and automatically imports previously finished data and merges it with newly digitised data. metaDigitise() also allows users to check their calibration along with editing previous digitisations.
 #' @param dir the path name to the directory / folder where the files are located
 #' @param import_all Logical - whether to import already completed files, defaults to FALSE (i.e. only import new files)
+#' @param image_name Vector of specific images for shinyDigitise to plot. defaults to NULL, i.e. all or only not completed images are input. If specified, overrides import_all
 
-shinyDigitise <- function(dir, import_all=FALSE){
+shinyDigitise <- function(dir, import_all=FALSE, image_name=NULL){
 
 
 	if( (substring(dir, nchar(dir)) == "/") == FALSE){
 	    dir <- paste0(dir, "/")
 	  }
 	  setup_calibration_dir(dir)
+# dir="~/Downloads/Image/"
 
-  if(import_all){
-  	details <- dir_details(dir)
-  }else{
- 	# done_details <- dir_details(dir)
-		details <- get_notDone_file_details(dir) 	
-  }
+	if(is.null(image_name)){
+	  if(import_all){
+	  	details <- dir_details(dir)
+	  }else{
+			details <- get_notDone_file_details(dir) 	
+	  }	
+	}else{
+		details <- dir_details(dir)
+
+		if(!image_name %in% details$name){
+			stop("image_name must refer to images within the directory")
+		}
+		details$paths <- details$paths[match(image_name,details$name)]
+		details$name <- image_name
+	}
+
+
+
+
+
+
+
+
 
 	counter_total <- length(details$paths)
 	# filename(details$paths[1])
