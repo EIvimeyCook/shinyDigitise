e66ab5748525f420082c07a238eefe5673c308a6
Joel Pick <joel.l.pick@gmail.com>
2023-01-07 19:30:39 +0000

    changed check_extract back to something that makes sense :)

--- R/check_functions.R
+++ R/check_functions.R
@@ -17,25 +17,23 @@ check_calibrate <- function(x){
 	}
 }
 
-check_extract <- function(x) {
-  if(is.null(x$raw_data)) {
+check_extract <- function(x){
+  if(is.null(x$raw_data)){
     FALSE
-  } else{
-
-  group_lengths <- table(x$raw_data$id)
-
-  print(group_lengths)
-  if(x$plot_type %in% c("mean_error","xy_mean_error","boxplot", "histogram", "scatterplot")) {
-    if(length(group_lengths) < 1) {
+  }else{
+    group_lengths<-unique(table(x$raw_data$id))
+    if(is.null(x$error_type) & x$plot_type %in% c("mean_error","xy_mean_error")){
       FALSE
-    } else {
-      if((x$plot_type == "mean_error" & (TRUE %in% (group_lengths != 2)) & is.null(x$error_type)) |
-         (x$plot_type == "xy_mean_error" & (TRUE %in% (group_lengths != 3)) & is.null(x$error_type)) |
-         (x$plot_type == "boxplot" & (TRUE %in% (group_lengths != 5)))) {
+    }else  if(x$plot_type %in% c("mean_error","xy_mean_error","boxplot")){
+      if(length(group_lengths)>1){
         FALSE
-      } else {
-        TRUE
+      }else{
+        (x$plot_type=="mean_error" & group_lengths==2)|
+        (x$plot_type=="xy_mean_error" & group_lengths==3)|
+        (x$plot_type=="boxplot" & group_lengths==5)
       }
+    }else{
+      TRUE
     }
   }
 }
