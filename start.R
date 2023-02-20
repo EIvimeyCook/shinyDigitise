6a9e4cbe55310494a143072691a67c09a223116a
Ed Ivimey-Cook <e.ivimeycook@gmail.com>
2023-02-20 07:37:12 +0000

    Delete start.R

--- start.R
+++
@@ -1,9 +0,0 @@
-
-rm(list=ls())
-devtools::install_github(repo = "EIvimeyCook/ShinyDigitise@InDev", force = TRUE)
-library(shinyDigitise)
-dat<-shinyDigitise(dir = "~/Desktop")
-
-devtools::load_all("~/Library/CloudStorage/OneDrive-UniversityofGlasgow/AllFiles/Other Projects/R Packages/ShinyDigitise/R")
-library(shinyDigitise)
-dat<-shinyDigitise(dir =  "~/Desktop") 
