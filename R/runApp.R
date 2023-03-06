088dc46fc634726d5de462ae24b8b5049ac1d24d
Ed Ivimey-Cook <e.ivimeycook@gmail.com>
2023-03-06 13:32:39 +0000

    Add files via upload

--- R/runApp.R
+++ R/runApp.R
@@ -1,6 +1,14 @@
 #' @title shinyDigitise
 #' @description Single or batch processing of figures with .png, .jpg, .tiff, .pdf extensions within a set directory. metaDigitise() consolidates the data and exports the data for each image and image type. It can also summarise the data, provide the raw data (if scatterplots) and automatically imports previously finished data and merges it with newly digitised data. metaDigitise() also allows users to check their calibration along with editing previous digitisations.
 #' @param dir the path name to the directory / folder where the files are located /can be left blank
+#' @examples 
+#' \donttest{ 
+#' # specific folder given using "dir"
+#' dataextraction <- shinyDigitise(dir = "~/Desktop/fig_folder")
+#'
+#' # no folder given, opens up an interactive window
+#' dataextraction <- shinyDigitise()
+#'}
 #' @export
 
 
