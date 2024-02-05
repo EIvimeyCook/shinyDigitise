1b044f94707ca658b32af49d882276e6fe96b34f
Ed Ivimey-Cook <e.ivimeycook@gmail.com>
2024-02-05 19:39:49 +0000

    Removed NoRd
    Removed NoRd

--- R/shinyDigitise_functions.R
+++ R/shinyDigitise_functions.R
@@ -3,7 +3,6 @@
 #' @title check_plottype
 #' @description Checks whether the plottype has been selected
 #' @param x list data created in sDigitise about the graph
-#' @noRd
 check_plottype <- function(x){
 	!is.null(x$plot_type)
 }
@@ -11,7 +10,6 @@ check_plottype <- function(x){
 #' @title check_orientation
 #' @description Checks whether a graph has been orientated
 #' @param x list data created in sDigitise about the graph
-#' @noRd
 check_orientation <- function(x){
 	TRUE
 }
@@ -19,7 +17,6 @@ check_orientation <- function(x){
 #' @title check_calibration
 #' @description Checks whether a graph has been calibrated
 #' @param x list data created in sDigitise about the graph
-#' @noRd
 check_calibrate <- function(x){
   if( is.null(x$calpoints) || is.null(x$variable) || is.null(x$point_vals)|| is.null(x$log_axes) ){
 	  FALSE
@@ -35,7 +32,6 @@ check_calibrate <- function(x){
 #' @title check_extract
 #' @description Checks whether a graph has been extracted
 #' @param x list data created in sDigitise about the graph
-#' @noRd
 check_extract <- function(x) {
   if(is.null(x$raw_data)) {
     FALSE
