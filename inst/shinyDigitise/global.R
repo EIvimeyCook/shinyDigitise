94c84c03b4990bda16c8a4787d1f4af845404c3a
Ed Ivimey-Cook <e.ivimeycook@gmail.com>
2024-02-21 11:38:36 +0000

    Update global.R

--- inst/shinyDigitise/global.R
+++ inst/shinyDigitise/global.R
@@ -1,7 +1,7 @@
 #create global variables for storing
 utils::globalVariables(c("details", "counter_total", "image_name", "id", "pch", "n"))
 
-#load internal functions#
-source("shinyDigitise_functions.R")
-source("metaDigitise_functions.R")
+#load internal functions
+source("shinyDigitise_functions.R", local = TRUE)
+source("metaDigitise_functions.R", local = TRUE)
 
